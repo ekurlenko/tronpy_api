67009e0c595711ba12bebf527e4da4b76975e76f
bublik911 <ekurlenko17@gmail.com>
2025-07-28 19:29:16 +0300

    router.py

--- app/api/tron/router.py
+++ app/api/tron/router.py
@@ -1,11 +1,59 @@
-from fastapi import APIRouter
+from typing import Annotated, List
+
+from fastapi import APIRouter, HTTPException, Depends
+from tronpy import Tron
+from tronpy.exceptions import AddressNotFound, BadAddress
+from tronpy.providers import HTTPProvider
+
+from app.repositories import WalletRepository, get_wallet_repository
+from app.schemas import Address, AccountData, ExceptionMessage, WalletsResponse
+
+provider = HTTPProvider(api_key="c2af4e33-9cd5-4fdb-a5fc-44ceb7d7ed8b")
+
+client = Tron(provider=provider, network="shasta")
 
 router = APIRouter(prefix="/tron")
 
-@router.get("/")
-def read_requests():
-    return {"status": "ok"}
 
-@router.post("/info")
-def get_info_by_address(address: str):
-    return {"status": "ok"}
+@router.get("/")
+def read_wallets(
+        offset: Annotated[int, 0],
+        limit: Annotated[int, 100],
+        wallet_repository: Annotated[WalletRepository, Depends(get_wallet_repository)]) -> List[WalletsResponse]:
+    wallets = wallet_repository.get_wallets(offset=offset, limit=limit)
+    response = [
+        WalletsResponse(id=wallet.id, address=wallet.address) for wallet in wallets
+    ]
+    return response
+
+
+@router.post("/info",
+             responses={
+                 400: {"model": ExceptionMessage},
+                 404: {"model": ExceptionMessage},
+                 200: {"model": AccountData},
+             })
+def info_by_address(
+        data: Annotated[Address, Address(address="")],
+        wallet_repository: Annotated[WalletRepository, Depends(get_wallet_repository)]) -> AccountData:
+    try:
+        response = AccountData(
+            balance=client.get_account_balance(data.address),
+            bandwidth=client.get_bandwidth(data.address),
+            energy=client.get_energy(data.address),
+        )
+        wallet_repository.create_wallet(data)
+
+    except AddressNotFound:
+        raise HTTPException(
+            status_code=404,
+            detail="Address not found"
+        )
+
+    except BadAddress:
+        raise HTTPException(
+            status_code=400,
+            detail="Bad address"
+        )
+
+    return response
