af7a8eae6aa95bfc39b213aae0bbd4a6c14e6cb7
bublik911 <ekurlenko17@gmail.com>
2025-07-28 16:21:04 +0300

    git ignore

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from app.api.router import router as api_router
 app = FastAPI()
+app.include_router(api_router)
 
 @app.get("/", include_in_schema=False)
 def main_route():
