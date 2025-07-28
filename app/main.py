f1cef9351ed105aca2f50b64bf4af87e51fac543
bublik911 <ekurlenko17@gmail.com>
2025-07-28 19:28:49 +0300

    create tables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from app.database import create_db
 from app.api.router import router as api_router
+
 app = FastAPI()
 app.include_router(api_router)
+
+
+@app.on_event("startup")
+def startup():
+    create_db()
+
 
 @app.get("/", include_in_schema=False)
 def main_route():
